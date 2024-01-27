8bda1a5fbb2deab1eaca4c3761d9cbb8e587eaaa
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2024-01-27 11:54:18 -0300

    Removed a bugged file

--- src/newFile.ts
+++
@@ -1,8 +0,0 @@
-import { UploadService } from './services/StorageService';
-import { app } from '.';
-
-app.post('/upload', async (request) => {
-  const uploadService = new UploadService();
-
-  return await uploadService.saveFile(request);
-});
